a032d010d17b5fd140ab0dd49adce502e83ba17d
Polina Puchek <polinapuchek89@gmail.com>
2024-02-03 13:13:43 +0200

    kvadrat

--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ def f(n):
         return 1
     else:
         return n * f(n - 1)
+def kvadrat(file): #b
+    counter = 0
+    for n in file:
+        if float(n) == (int(float(n)**0.5))**2 and float(n) % 2 != 0:
+            counter += 1
+    return counter
